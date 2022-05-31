8f02cc40997d967d05a2d4088b1308f33e35f3b4
kaboyokelvin <99651351+kaboyokelvin@users.noreply.github.com>
2022-06-01 01:31:52 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if (letter == 101 or letter == 133):
-        continue:
-    print("{}".format(chr(letter)), end=" ")
+for a in range(97, 97 + 26):
+    if a == 113 or a == 101:
+        continue
+    print("{:c}".format(a), end='')
+
+
